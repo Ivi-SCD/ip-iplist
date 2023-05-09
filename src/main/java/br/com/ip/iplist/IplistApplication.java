672ff54388b1c00b04167d1f391ff9651aa987cb
Ivi-SCD <ivipnascimento@hotmail.com>
2023-05-08 21:26:49 -0300

    First Commit

---
+++ src/main/java/br/com/ip/iplist/IplistApplication.java
@@ -0,0 +1,13 @@
+package br.com.ip.iplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplistApplication.class, args);
+	}
+
+}
